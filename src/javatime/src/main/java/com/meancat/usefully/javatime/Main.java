40c5b4fb500d1bc4d82a7ecd53a539bb117e6e06
Tony Rasa <trasa@meancat.com>
2016-06-21 17:07:13 -0700

    Javatime - Java, what time is it?
    Finally got sick of writing this snippet over and over, so here's a jar
    that does it. /shrug

---
+++ src/javatime/src/main/java/com/meancat/usefully/javatime/Main.java
@@ -0,0 +1,11 @@
+package com.meancat.usefully.javatime;
+
+/**
+ * Print the System.currentTimeMillis()
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
